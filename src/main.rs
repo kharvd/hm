3202c78f23ddcbf393d4804b4da579f3253a19e4
Valery Kharitonov <val@kharvd.com>
2023-09-04 13:11:46 -0400

    add memory tracking

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,31 @@ mod prelude;
 mod typing;
 mod value;
 
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
+
+struct Counter;
+
+static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+
+unsafe impl GlobalAlloc for Counter {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let ret = System.alloc(layout);
+        if !ret.is_null() {
+            ALLOCATED.fetch_add(layout.size(), Relaxed);
+        }
+        ret
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+        ALLOCATED.fetch_sub(layout.size(), Relaxed);
+    }
+}
+
+#[global_allocator]
+static A: Counter = Counter;
+
 fn main() {
     let mut env = Env::prelude();
 
@@ -62,11 +87,12 @@ fn eval_line(env: &Env, line: &str) -> Result<(String, Env), String> {
 
             (
                 format!(
-                    "{} : {}\ntc: {}ms\neval: {}ms",
+                    "{} : {}\ntc: {}ms\neval: {}ms\nmemory: {} bytes",
                     res,
                     type_expr,
                     type_checking_time.as_millis(),
-                    evaluation_time.as_millis()
+                    evaluation_time.as_millis(),
+                    ALLOCATED.load(Relaxed)
                 ),
                 env.clone(),
             )
